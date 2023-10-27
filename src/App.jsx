a48c102c85bfbd1768560849f09692fe6151f0b2
Tanushree227 <tanushreedas813@gmail.com>
2023-10-27 18:34:25 +0530

    Added contents related to main page.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>OOPS Concepts in Programming</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+          OOPS refers to languages that use objects in programming, they use objects as a primary source to implement what is to happen in the code.
+          <br /> Let's deep dive to know more....
         </p>
       </header>
       <main>
