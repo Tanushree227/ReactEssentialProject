4bd109529dfa4bb408ca230eee25fc2726450a1e
Tanushree227 <tanushreedas813@gmail.com>
2023-10-27 18:42:04 +0530

    created custom Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+function Header() {
+  return (<header>
+    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <h1>OOPS Concepts in Programming</h1>
+    <p>
+      OOPS refers to languages that use objects in programming, they use objects
+      as a primary source to implement what is to happen in the code.
+      <br /> Let's deep dive to know more....
+    </p>
+  </header>);
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>OOPS Concepts in Programming</h1>
-        <p>
-          OOPS refers to languages that use objects in programming, they use objects as a primary source to implement what is to happen in the code.
-          <br /> Let's deep dive to know more....
-        </p>
-      </header>
+    <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
