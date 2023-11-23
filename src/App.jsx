7a40a1988e1614872bf6d07763fda03938cb9dea
Tanushree227 <tanushreedas813@gmail.com>
2023-11-23 18:59:22 +0530

    dynamically generated content

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,68 @@
-import { useState } from 'react';
-import { CORE_CONCEPTS } from './data';
-import Header from './components/Header.jsx'; 
-import CoreConcept from './components/CoreConcepts.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data';
+import { useState } from "react";
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data";
 
 function App() {
-  
-  const [ selectedTopic, setSelectedTopic ] = useState();
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton)
-  {
+  function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
 
   let tabContent = <p>Please select a topic.</p>;
 
-  if(selectedTopic)
-  {
+  if (selectedTopic) {
     tabContent = (
-    <div id='tab-content'>
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>{EXAMPLES[selectedTopic].code}</code>
-      </pre>
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
       </div>
-  )
+    );
   }
 
   return (
     <div>
-    <Header />
+      <Header />
       <main>
-        <section id='core-concepts'>
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]} />
-          <CoreConcept {...CORE_CONCEPTS[1]} />
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
-        </ul>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept {...conceptItem} />)}
+          </ul>
         </section>
-        <section id='examples'>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('encapsulation')}>Encapsulation</TabButton>
-            <TabButton onSelect={() => handleSelect('abstraction')}>Abstraction</TabButton>
-            <TabButton onSelect={() => handleSelect('inheritance')}>Inheritance</TabButton>
-            <TabButton onSelect={() => handleSelect('polymorphism')}>Polymorphism</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "encapsulation"}
+              onSelect={() => handleSelect("encapsulation")}
+            >
+              Encapsulation
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "abstraction"}
+              onSelect={() => handleSelect("abstraction")}
+            >
+              Abstraction
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "inheritance"}
+              onSelect={() => handleSelect("inheritance")}
+            >
+              Inheritance
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "polymorphism"}
+              onSelect={() => handleSelect("polymorphism")}
+            >
+              Polymorphism
+            </TabButton>
           </menu>
           {tabContent}
         </section>
